cde0afa231fc6598561785bde05ff79583f6f368
Viacheslav <baficvn@gmail.com>
2022-07-12 17:06:04 +0300

    change same error

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,7 @@
 #!/usr/bin/env groovy
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+     sh "docker build -t akakum/java-maven-app:lst  . "
+     sh 'docker image ls'
     }
-} 
+ 
