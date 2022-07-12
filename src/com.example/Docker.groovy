ebd02557bca4ac3fcb4cef9f8be02c9ef64de5bb
Viacheslav <baficvn@gmail.com>
2022-07-12 17:50:59 +0300

    add new class file

---
+++ src/com.example/Docker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+package com.example
+class Docker implements Serializable{
+    def script
+    Docker(script){
+        this.script = script
+    }
+    def BuildDockerImage(String imageName){ // imageName is akakum/java-maven-app:lst
+        script.echo "building the docker image..."
+        script.sh "docker build -t  $imageName . "
+        script.sh 'docker image ls'
+    }
+   /* 
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) 
+    {
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push akakum/java-maven-app:lst'
+    }
+    */
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
-def call() {
-    echo "building the docker image..."
-     sh "docker build -t akakum/java-maven-app:lst  . "
-     sh 'docker image ls'
+import com.example.Docker
+def call(String imageName) {
+    return new Docker(this).buildImage(imageName)
     }
  
