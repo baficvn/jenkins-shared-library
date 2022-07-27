0955fccb08606937d1fe416af76902c5f060a957
Viacheslav <baficvn@gmail.com>
2022-07-27 21:50:11 +0300

    +

--- vars/deployImage.groovy
+++ vars/deployImage.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
-def call(){
+def call(String imageName){
     echo 'Deploy Docker Image'
     withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) 
     {
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        //sh 'docker push akakum/java-maven-app:lst'
-        sh 'docker image ls'
+        sh "docker image tag $imageName akakum/java-maven-app:latest"
+        sh 'docker push akakum/java-maven-app:latest'
+        
     }
 }
