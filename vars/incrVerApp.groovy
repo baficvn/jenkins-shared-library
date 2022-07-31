7a4ee10353781ea0a5aef58183cbc9d566423c74
Viacheslav <baficvn@gmail.com>
2022-07-31 15:04:28 +0300

    +

--- vars/incrVerApp.groovy
+++ vars/incrVerApp.groovy
@@ -4,6 +4,5 @@ def call(){
     String command = "mvn build-helper:parse-version versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit"
                     sh(command)
                     //def matcher = readFile('pom.xml') =~ '<version>(.+)<version>'
-    
                     //echo "$IMAGE_NAME"
 }
