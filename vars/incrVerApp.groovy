c145a658183cc93caefe2c14cd682bb5595c02ea
Viacheslav <baficvn@gmail.com>
2022-07-31 15:19:49 +0300

    +

--- vars/incrVerApp.groovy
+++ vars/incrVerApp.groovy
@@ -3,7 +3,11 @@ def call(BUILD_NUMBER){
     echo "Increment Version Application in pom.xml file ${BUILD_NUMBER}"
     String command = "mvn build-helper:parse-version versions:set -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit"
                     sh(command)
+                    pom = readMavenPom file: 'pom.xml'
+                    pom.version
+                    def version = pom.version
+                    env.IMAGE_NAME = "$version-$BUILD_NUMBER"
                     //def matcher = readFile('pom.xml') =~ '<version>(.+)<version>'
                     //echo "$IMAGE_NAME"
-                    return "${BUILD_NUMBER}"
+                    return "${IMAGE_NAME}"
 }
